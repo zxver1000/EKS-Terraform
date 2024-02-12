916ae2eedca9bf30b3d07d51503c065c78ed0b84
zxver1000 <wlwhs5014@naver.com>
2024-02-12 21:04:23 +0900

    add version.tf

---
+++ version.tf
@@ -0,0 +1,27 @@
+locals {
+    default_tags = {
+        Terraform = "true"
+    }
+}
+
+# 필요한 provider가 있다면 적절히 추가하여 사용합니다.
+terraform {
+    required_version = "1.5.7"
+
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "5.34.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "ap-northeast-2"
+    
+    default_tags {
+        tags = local.default_tags
+    }
+}
+
+
